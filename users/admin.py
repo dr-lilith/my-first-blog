b9beac5c449f5fcaf69612573087a75e08d48575
Irina <ireana.melnikova@gmail.com>
2022-05-17 17:21:00 +0300

    Registered model in admin.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_active', 'is_staff', 'date_joined', 'description', 'avatar')
+    list_display_links = ('email', 'username')
+    search_fields = ('email', 'username')
+    list_editable = ('first_name', 'last_name', 'is_active', 'is_staff', 'date_joined', 'description', 'avatar')
+
+
+admin.site.register(User, UserAdmin)
