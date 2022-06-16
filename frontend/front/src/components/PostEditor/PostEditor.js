f0587fea5cf765df5c580455c7af817139c7674c
Irina <ireana.melnikova@gmail.com>
2022-06-16 17:19:39 +0300

    Btn design added

--- frontend/front/src/components/PostEditor/PostEditor.js
+++ frontend/front/src/components/PostEditor/PostEditor.js
@@ -9,10 +9,8 @@ const PostEditor=( { postData })=> {
     }
     return(
         <div className={styles.PostEditor}>
-            <h1>
-                {'Сoздание нового поста'}
-            </h1>
-            
+            <h1 className={styles.CreateNewPost}>Создание нового поста:</h1>
+            <textarea rows="2" cols="120" name="textArea"></textarea>
             <textarea rows="20" cols="120" name="textArea"></textarea>
             <p>
                 <button className={styles.btn}>Coхранить навый пост</button>
