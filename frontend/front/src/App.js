78da4b02d3c51291d9d4281b188e96a3f066c6a7
Irina <ireana.melnikova@gmail.com>
2022-06-18 16:06:48 +0300

    input for adding tags added.

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import PostEditor from "./components/PostEditor/PostEditor";
 import PostsContainer from "./components/PostsContainer/PostsContainer"
 import UserProfile from "./components/UserProfile/UserProfile";
+import './App.css'
 function App() {
   return (
     <div className="App">
--- frontend/front/src/components/PostEditor/PostEditor.js
+++ frontend/front/src/components/PostEditor/PostEditor.js
@@ -1,20 +1,22 @@
 import styles from "./PostEditor.module.css"
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const PostEditor=( { postData })=> {
-    const style = {
-        border: '1px solid #ccc',
-        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)'
+    const [isSaved, setIsSaved] = useState(false)
+    const savedHandler = ()=>{
+        setIsSaved(true)
     }
     return(
         <div className={styles.PostEditor}>
             <h1 className={styles.CreateNewPost}>Создание нового поста:</h1>
-            <textarea rows="2" cols="120" name="textArea"></textarea>
-            <textarea rows="20" cols="120" name="textArea"></textarea>
+            <input placeholder="Заголовок поста" className={styles.headlineInput}/>
+            <textarea rows="20" cols="120" name="textArea" className={styles.postTextarea} placeholder='Текст поста'/>
+            <input placeholder="Загрузите картинку" type='file' onClick={(e)=>console.log(e.target.value)}/>
             <p>
-                <button className={styles.btn}>Coхранить навый пост</button>
+                <button className={!isSaved ? styles.btn: styles.btnSaved} onClick={savedHandler}>{!isSaved ? 'Coхранить новый пост':'Сохранено'}</button>
             </p>
+            {isSaved && <input placeholder="добавление тегов"/>}
         </div> 
 
     );
