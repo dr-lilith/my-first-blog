dc2e36c806fa86e59034a23d028d93bd05719af8
Irina <ireana.melnikova@gmail.com>
2022-06-28 03:59:08 +0300

    Added CKEditor to NewPost component

--- frontend/front/src/components/NewPost/NewPost.js
+++ frontend/front/src/components/NewPost/NewPost.js
@@ -2,19 +2,42 @@ import styles from "./NewPost.module.css"
 import React, { useState } from 'react'
 import TagInputComponent from "./TagInputComponent/TagInputComponent"
 import ImageUploader from "./ImageUploader/ImageUploader"
+import { httpPost } from "../utils/httpClient"
+
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 
 const NewPost=( { postData })=> {
     const [isSaved, setIsSaved] = useState(false)
     const savedHandler = ()=>{
+        httpPost("/posts/create", )
         setIsSaved(true)
     }
     return(
         <div className={styles.PostEditor}>
             <h1 className={styles.CreateNewPost}>Создание нового поста:</h1>
             <input placeholder="Заголовок поста" className={styles.headlineInput}/>
-            <textarea rows="20" cols="120" name="textArea" className={styles.postTextarea} placeholder='Текст поста'/>
-            <ImageUploader/>
+            
+            <CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+            
             <p>
                 <button className={!isSaved ? styles.btn: styles.btnSaved} onClick={savedHandler}>{!isSaved ? 'Coхранить новый пост':'Сохранено'}</button>
             </p>
